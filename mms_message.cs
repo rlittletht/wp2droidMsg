c0005a0ee42ebf62f8e37d9f90d7f9143d781aef
Rob Little <rlittletht@users.noreply.github.com>
2018-09-20 20:24:48 -0700

    add mms_message type

---
+++ mms_message.cs
@@ -0,0 +1,25 @@
+﻿
+namespace wp2droidMsg
+{
+    public class MmsPart
+    {
+        private int m_nSeq;
+        private string m_sCt;
+        private string m_sName;
+        private string m_sChset;
+        private string m_sCd;
+        private string m_sFn;
+        private string m_sCid;
+        private string m_sCl;
+        private string m_sCtt_s;
+        private string m_sCtt_t;
+        private string m_sText;
+        private string m_sData; //optional
+
+    }
+    public class MmsMessage
+    {
+        // from the schema at http://synctech.com.au/wp-content/uploads/2018/01/sms.xsd_.txt
+
+    }
+}
