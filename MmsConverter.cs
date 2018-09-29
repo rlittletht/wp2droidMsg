0adb8413b5c37c65b42dd5b25dfc2cc7e2118651
Rob Little <rlittletht@users.noreply.github.com>
2018-09-28 21:03:11 -0700

    refactor message reading into TCore.MMS

--- MmsConverter.cs
+++ MmsConverter.cs
@@ -14,63 +14,14 @@ namespace wp2droidMsg
 
         public void Convert(string sInFile, string sOutFile)
         {
-            XmlReader xr = XmlReader.Create(sInFile);
+            List<WpMessage> mmses;
 
-            if (!xr.Read())
-                return;
-
-            List<WpMessage> mmses = new List<WpMessage>();
-
-
-            bool fFoundMessageArray = false;
-            bool fValidExit = false;
-
-            while (true)
+            using (XmlReader xr = XmlReader.Create(sInFile))
             {
-                XmlNodeType nt = xr.NodeType;
+                mmses = WpMessage.ReadMessagesFromXml(xr);
 
-                if (nt == XmlNodeType.Element)
-                {
-                    if (!fFoundMessageArray && xr.Name == "ArrayOfMessage")
-                    {
-                        xr.ReadStartElement();
-                        fFoundMessageArray = true;
-                        continue;
-                    }
-
-                    // only other valid element is message
-                    if (xr.Name != "Message")
-                        throw new Exception($"Illegal element {xr.Name} under ArrayOfMessages");
-
-                    mmses.Add(WpMessage.CreateFromXmlReader(xr));
-                    continue;
-                }
-
-                if (nt == XmlNodeType.EndElement)
-                {
-                    if (xr.Name == "ArrayOfMessage")
-                    {
-                        fValidExit = true;
-                        break; // yay done
-                    }
-
-                    throw new Exception($"end element {xr.Name} unexpected");
-                }
-
-                if (nt == XmlNodeType.Attribute)
-                    throw new Exception($"attribute unexpected per schema");
-
-                // all others are just skipped
-                if (!xr.Read())
-                    break;
+                xr.Close();
             }
-
-            if (!fValidExit)
-                throw new Exception("end of file before end message array");
-
-            // at this point, we have all the SMS messages ready to write
-            xr.Close();
-            xr.Dispose();
 
             // ok, now we have a collection of messages...now we have to write them out...
             XmlWriterSettings xws = new XmlWriterSettings();
--- SmsConverter.cs
+++ SmsConverter.cs
@@ -14,62 +14,14 @@ namespace wp2droidMsg
 
         public void Convert(string sInFile, string sOutFile)
         {
-            XmlReader xr = XmlReader.Create(sInFile);
+            List<SmsMessage> smses;
 
-            if (!xr.Read())
-                return;
-
-            List<SmsMessage> smses = new List<SmsMessage>();
-
-            bool fFoundMessageArray = false;
-            bool fValidExit = false;
-
-            while (true)
+            using (XmlReader xr = XmlReader.Create(sInFile))
             {
-                XmlNodeType nt = xr.NodeType;
-
-                if (nt == XmlNodeType.Element)
-                {
-                    if (!fFoundMessageArray && xr.Name == "ArrayOfMessage")
-                    {
-                        xr.ReadStartElement();
-                        fFoundMessageArray = true;
-                        continue;
-                    }
-
-                    // only other valid element is message
-                    if (xr.Name != "Message")
-                        throw new Exception($"Illegal element {xr.Name} under ArrayOfMessages");
-
-                    smses.Add(SmsMessage.CreateFromWindowsPhoneXmlReader(xr));
-                    continue;
-                }
-
-                if (nt == XmlNodeType.EndElement)
-                {
-                    if (xr.Name == "ArrayOfMessage")
-                    {
-                        fValidExit = true;
-                        break; // yay done
-                    }
-
-                    throw new Exception($"end element {xr.Name} unexpected");
-                }
-
-                if (nt == XmlNodeType.Attribute)
-                    throw new Exception($"attribute unexpected per schema");
-
-                // all others are just skipped
-                if (!xr.Read())
-                    break;
+                smses = SmsMessage.ReadMessagesFromWpXml(xr);
+                // at this point, we have all the SMS messages ready to write
+                xr.Close();
             }
-
-            if (!fValidExit)
-                throw new Exception("end of file before end message array");
-
-            // at this point, we have all the SMS messages ready to write
-            xr.Close();
-            xr.Dispose();
 
             XmlWriter xw = XmlTextWriter.Create(sOutFile);
 
